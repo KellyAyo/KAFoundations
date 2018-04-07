0d9646f9331c535c58e1fcbb9ced42fe2152853b
Phillip Kelly-Ayo <Kelly-Ayo@MacBook-Pro.local>
2018-04-07 13:53:31 +0100

    Initial commit

---
+++ KAFoundations/KAFoundations.h
@@ -0,0 +1,19 @@
+//
+//  KAFoundations.h
+//  KAFoundations
+//
+//  Created by Phillip Kelly-Ayo on 07/04/2018.
+//  Copyright © 2018 Phillip Kelly-Ayo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KAFoundations.
+FOUNDATION_EXPORT double KAFoundationsVersionNumber;
+
+//! Project version string for KAFoundations.
+FOUNDATION_EXPORT const unsigned char KAFoundationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KAFoundations/PublicHeader.h>
+
+
